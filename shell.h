68c628829abd28460e07e4b58df964e8dd0d07d4
01Eddie <jesus.11.97@hotmail.com>
2021-04-07 11:12:12 -0500

    [ADD] shell.h

--- shell.h
+++ shell.h
@@ -17,7 +17,7 @@ char **re_alloc(char **ptr, int *size);
 /*String Function*/
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
-int strcmp(const char *dest, const char *src);
+int _strcmp(const char *dest, const char *src);
 char *_strdup(char *s);
 char *_strcat(char *str1, char *str2);
 char **split_line(char *line, char *delim);
@@ -25,5 +25,6 @@ char **re_alloc(char **ptr, int *size);
 int pr_exec(char **args);
 char *_getenv(char *name);
 char *read_line(void);
+void ctrlC(int sig_num);
 
 #endif
