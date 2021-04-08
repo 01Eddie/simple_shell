07b7e2402ac7488ed329625c859f8a8f97876331
Alex Rivera <alexriveracruz4@gmail.com>
2021-04-08 16:16:14 +0000

    [modify] correction for betty style

--- signal.c
+++ signal.c
@@ -1,10 +1,11 @@
 #include "shell.h"
+
 /**
  * ctrlC - function of detected ctrl + c or ANSI C signal handling
  * @sig_num: Integer
+ *
  * Return: void
  */
- 
 void ctrlC(int sig_num)
 {
 	signal(sig_num, SIG_IGN);
