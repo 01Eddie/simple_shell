b1c3eda2501606d73e6e4a6dbed5598c07e307ef
Alex Rivera <alexriveracruz4@gmail.com>
2021-04-13 04:31:11 +0000

    [ADD] adding functions to header

--- shell.h
+++ shell.h
@@ -7,7 +7,6 @@ extern char *program_invocation_name;
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
